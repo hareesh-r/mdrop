387dea924572769a9a5d3e1069b34c1c9213264f
hareesh <hareeshraji708@gmail.com>
2020-11-13 12:03:20 +0530

    added faq

---
+++ assets/js/faq.js
@@ -0,0 +1,17 @@
+(function($) {
+	'use strict';
+	
+	jQuery(document).on('ready', function(){
+	
+			$('a.page-scroll').on('click', function(e){
+				var anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top - 50
+				}, 1500);
+				e.preventDefault();
+			});		
+
+	}); 	
+
+				
+})(jQuery);
